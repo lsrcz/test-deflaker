9a14de6fe97b32b0cbe3f62f4484f77f971375f8
Sirui Lu <lsrcz@pku.edu.cn>
2019-03-05 02:50:31 +0800

    Update data

--- ninjarun-old.sh
+++ ninjarun-old.sh
@@ -18,19 +18,11 @@ setrepo() {
 }
 
 setrepo $1
-mvn clean
 mvn install
-mvn clean
 mvn verify 2>&1 > $1.log
 if (grep 'There are test failures' $1.log)
 then
     mv $1.log ../logs/failed-$1.log
-    setrepo "$1^"
-    mvn clean
-    mvn install
-    mvn clean
-    mvn verify 2>&1 > $1.log
-    mv $1.log ../logs/former-$1.log
 else
     mv $1.log ../logs/ok-$1.log
 fi
